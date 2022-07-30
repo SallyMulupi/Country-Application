6d82469b2164f114c471aac821b8c85cc1e939cd
SallyMulupi <mulupisally2gmail.com>
2022-07-31 00:18:39 +0300

    add country

--- src/components/pages/Country.js
+++ src/components/pages/Country.js
@@ -1,4 +1,19 @@
+import React from 'react'
+import { useState } from 'react';
+import { useEffect } from 'react'
+
+
+
 function Country() {
-return <div> Country</div>;
-}
-export default Country;
+  const [isLoading,setIsLoading]=useState(true);
+  const [loadedCountry,setLoadedMovies]=useState([]);
+  
+        useEffect(() => {
+           setIsLoading(true)
+          fetch('https://restcountries.eu')
+
+          .then(response => {
+           return response.json();
+          })
+       
+          .then(data => {
