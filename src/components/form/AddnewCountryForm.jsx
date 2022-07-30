66be7232b46a9447dbfbddf522af7d6001fb66ca
SallyMulupi <mulupisally2gmail.com>
2022-07-30 23:56:13 +0300

    add function addnewcountryform

---
+++ src/components/form/AddnewCountryForm.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import {useRef} from 'react'
+import './addNewForm.css'
+function AddNewCountryForm(props) {
+    const continentInputRef = useRef();
+    const countryInputRef = useRef();
+    const capitalInputRef = useRef();
+    const currencyInputRef = useRef();
+    const languageInputRef = useRef();
+   
+
+export default AddNewcountryForm;
