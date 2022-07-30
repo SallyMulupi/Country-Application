66c5ee5588eaac09ddb439c5981eb17f3f8ac61d
SallyMulupi <mulupisally2gmail.com>
2022-07-30 23:45:32 +0300

    add links

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -3,8 +3,19 @@ import './nav.css'
 import { Link } from 'react-router-dom'
 
 function NavBar() {
-    return ( 
-        <header className='header>'
-       
-    )
+  return (
+    <header className='header'>
+        <div className='logo'>The World <span>Link</span></div>
+        <nav>
+            <ul>
+               <li><Link to='/'>Country</Link></li> 
+               <li> <Link to='/new-country'>New-Country</Link></li>
+               <li> <Link to='/favorites'>Favorite-Country</Link></li>
+            </ul>
+        </nav>
+    </header>
+    
+  )
 }
+
+export default NavBar
