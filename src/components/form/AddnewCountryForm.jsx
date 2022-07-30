718bfc6161133ba761bb03556b18524c948e4bae
SallyMulupi <mulupisally2gmail.com>
2022-07-30 23:59:15 +0300

    add submithandler

--- src/components/form/AddnewCountryForm.jsx
+++ src/components/form/AddnewCountryForm.jsx
@@ -8,6 +8,13 @@ function AddNewCountryForm(props) {
     const capitalInputRef = useRef();
     const currencyInputRef = useRef();
     const languageInputRef = useRef();
-   
+    function submitHandler(e){
+        e.preventDefault();
+
+         const enteredcontinent= continentInputRef.current.value;
+         const enteredCountry = countryInputRef.current.value;
+         const enteredCapital = capitalInputRef.current.value;
+         const enteredcurrency = currencyInputRef.current.value;
+         const enteredLanguage = languageInputRef.current.value;
 
 export default AddNewcountryForm;
