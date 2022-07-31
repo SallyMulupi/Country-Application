0247883712bea58d8fb0fc4f5c4149730d023ebb
SallyMulupi <mulupisally2gmail.com>
2022-07-31 10:41:06 +0300

    rename countryinfo

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {Route, BrowserRouter } from 'react-router-dom';
+import {
+    BrowserRouter,                                                                                                              
+    Routes,
+    Route,
+} from 'react-router-dom'
+import AllCountries from "./components/AllCountries/AllCountries";
+import CountryInfo from "./components/CountryInfo/CountryInfo";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <BrowserRouter>
+<Routes>
+<Route path="/" element={<AllCountries />} />
+ <Route path="/country/:countryName" element={<CountryInfo />} />
+</Routes>
     <App />
 </BrowserRouter>
  
